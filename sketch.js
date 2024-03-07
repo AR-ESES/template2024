780f08f6f2402685b37af91263151a054e466ccb
steam228 <steam228@gmail.com>
2024-03-07 11:39:31 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(1280, 1024);
+  background(153);
+  stroke(0);
+  strokeWeight(2);
+  fill(237, 28, 36);
+  circle(30, 30, 40);
+  line(30, 30, 1280 / 2, 1024 / 2);
+}
+
+function draw() {}
